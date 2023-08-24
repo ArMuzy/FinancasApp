3610100068aa6ffaea753cdabeeb9c1ce1edf115
ArMuzy <ar2526232@gmail.com>
2023-08-23 21:48:08 -0300

    first commit

---
+++ FinancasApp.Presentation/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FinancasApp.Presentation.Controllers
+
+    
+{
+    public class AccountController : Controller
+    {
+        /// <summary>
+        /// Método para abrir página  Account/Login
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Método para abrir página  Account/Register
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Método para abrir página  Account/ForgotPassword
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+
+
+    }
+}
